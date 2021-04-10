a4220228c5bd647e65ab486bd4fbe18b4c4d9000
Michael <mlarussa49@yahoo.com>
2021-04-10 16:38:54 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/audiotracks/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.audiotracks;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button popupButton;
+
+    public static final int TEXT_REQUEST = 1;
+    public static final String EXTRA_MESSAGE =
+            "com.example.audiotracks.extra.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void popupMenuExample(View view) {
+        popupButton = findViewById(R.id.popup);
+        PopupMenu p = new PopupMenu(MainActivity.this, popupButton);
+        p.getMenuInflater().inflate(R.menu.main_popup_menu, p .getMenu());
+        p.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                //When an Item in the drop down is pressed, we need to know which one is pressed.
+                if (item.getTitle().toString().equals("New Project")) {
+                    newProject();
+                    return true;
+                }
+                else if (item.getTitle().toString().equals("Sync From Cloud")) {
+                    cloudSync();
+                    return true;
+                }
+                else if (item.getTitle().toString().equals("Login")) {
+                    cloudLogin();
+                    return true;
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"didn't work", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            }
+        });
+        p.show();
+    }
+
+    //This will execute when the user presses New Project in the drop down
+    private void newProject() {
+        Toast.makeText(MainActivity.this,"new project", Toast.LENGTH_SHORT).show();
+        showAddProjectDialog(MainActivity.this);
+    }
+
+    //This will execute when the user presses Cloud Sync in the drop down
+    private void cloudSync() {
+        Toast.makeText(MainActivity.this,"cloud sync", Toast.LENGTH_SHORT).show();
+    }
+
+    //This will execute when the user presses Login in the drop down
+    private void cloudLogin() {
+        Toast.makeText(MainActivity.this,"cloud login", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showAddProjectDialog(Context c) {
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("Name your new Project:")
+                //.setMessage("What do you want to do next?")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //This Variable holds the string the user just entered. We will pass it to the second screen, where the user can choose to save the project or not, putting it into the data structure we choose for storing these projects.
+                        String task = String.valueOf(taskEditText.getText());
+                        Intent intent = new Intent(MainActivity.this, ProjectEditor.class);
+                        String message = taskEditText.getText().toString();
+                        intent.putExtra(EXTRA_MESSAGE, message);
+                        startActivityForResult(intent, TEXT_REQUEST);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/audiotracks/ProjectEditor.java
@@ -0,0 +1,93 @@
+package com.example.audiotracks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ProjectEditor extends AppCompatActivity {
+    Button popupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_editor);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Button mainButton = findViewById(R.id.project_button);
+        mainButton.setText(message);
+    }
+
+    public void playFunction(View view)
+    {
+        System.out.println("Playing Audio...");
+    }
+
+    public void stopFunction(View view)
+    {
+        System.out.println("Stopping Audio...");
+    }
+
+    public void recordFunction(View view)
+    {
+        System.out.println("Recording Audio...");
+    }
+
+    public void popupMenu(View view) {
+        popupButton = findViewById(R.id.project_button);
+        PopupMenu p = new PopupMenu(ProjectEditor.this, popupButton);
+        p.getMenuInflater().inflate(R.menu.project_popup_menu, p .getMenu());
+        p.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                //When an Item in the drop down is pressed, we need to know which one is pressed.
+                if (item.getTitle().toString().equals("Save")) {
+                    saveFunction();
+                    return true;
+                }
+                else if (item.getTitle().toString().equals("Export")) {
+                    exportFunction();
+                    return true;
+                }
+                else if (item.getTitle().toString().equals("Rename")) {
+                    renameFunction();
+                    return true;
+                }
+                else if (item.getTitle().toString().equals("Delete")) {
+                    deleteFunction();
+                    return true;
+                }
+                else {
+                    Toast.makeText(ProjectEditor.this,"didn't work", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            }
+        });
+        p.show();
+    }
+
+    public void saveFunction()
+    {
+        System.out.println("Saving...");
+    }
+
+    public void exportFunction()
+    {
+        System.out.println("Exporting...");
+    }
+
+    public void renameFunction()
+    {
+        System.out.println("Renaming...");
+    }
+
+    public void deleteFunction()
+    {
+        System.out.println("Deleting...");
+    }
+}
