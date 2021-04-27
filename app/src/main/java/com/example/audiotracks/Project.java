4c1517bfe543e0d96a6779c9de05164b11dabeaf
Michael <mlarussa49@yahoo.com>
2021-04-27 10:34:34 -0700

    utilized Project class properly

--- app/src/main/java/com/example/audiotracks/Project.java
+++ app/src/main/java/com/example/audiotracks/Project.java
@@ -2,39 +2,28 @@ package com.example.audiotracks;
 
 public class Project {
     String name = "";
-    String paths[] = {};
+    String paths[] = {null, null, null};
 
-    /*public void addTrack(Track track)
+    public void addPath(String path, int index)
     {
-        paths[index] = track;
-        index++;
+        paths[index] = path;
     }
 
-    public void removeTrack(Integer i)
+    public void removePath(int index)
     {
-        if (i == index - 1)
-        {
-            //We are at the top of the array, simply set the top to null and lower index.
-            paths[i] = null;
-            index--;
-        }
-        else if ( i < index - 1)
-        {
-            //Everything above needs to move down
-            for (int j = i; j < index; j++)
-            {
-                paths[j] = paths[j+1];
-            }
-            //The top one can now disappear, since it is now a copy of the one below it
-            paths[index - 1] = null;
-            index--;
-        }
-        else if ( i > index - 1)
-        {
-            //Don't do anything, we cannot remove this track, since it does not exist.
+        if (index >= 0 && index <=2) {
+            paths[index] = null;
         }
     }
-    */
+
+    public String getPath(int index)
+    {
+        if (index >= 0 && index <=2) {
+            return paths[index];
+        }
+        return "Error";
+    }
+
     public void setName(String newName)
     {
         name = newName;
