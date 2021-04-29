8df87220903cc02a00a851035cc57a7b4f6b27b2
ehanhan <78825648+ehanhan@users.noreply.github.com>
2021-04-28 21:36:21 -0700

    javadoc done

--- app/src/main/java/com/example/audiotracks/MainActivity.java
+++ app/src/main/java/com/example/audiotracks/MainActivity.java
@@ -42,7 +42,11 @@ import org.w3c.dom.Text;
  * First it runs onCreate which checks if the user is logged in
  * If the user is logged in then it takes the user to our main menu.
  * If the user is not logged in then they are taken to our sign in page engined through google.
- * @author ehabh
+ * @author Ehab Hanhan
+ * @author Michael LaRussa
+ * @author Koshiro Kawai
+ * @author Sahej Hundal
+ * @version 1.0
  */
 public class MainActivity extends AppCompatActivity {
     Button popupButton;
@@ -101,8 +105,8 @@ public class MainActivity extends AppCompatActivity {
         }
         myRef.child(mFirebaseAuth.getCurrentUser().getUid()).child("projects").addListenerForSingleValueEvent(new ValueEventListener() {
             /**
-             * onDataChange !!!!!INSERT DESCRIPTION HERE !!!!!!!!!!
-             * @param dataSnapshot !!!!!! INSERT DESCRIPTION HERE !!!!!!
+             * onDataChange searches for current projects on the signed in user's account and displays all of them
+             * @param dataSnapshot contains all of the user's projects
              */
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -128,8 +132,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
             /**
-             *  onCancelled !!!!!!! INSERT DESCCRIPTION HERE !!!!!!!!
-             * @param databaseError !!!!!! INSERT DESCRIPTION HERE !!!!!
+             *  onCancelled error handling
+             * @param databaseError error handling
              */
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -139,8 +143,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * popupMenuExample creates
-     * @param view
+     * popupMenuExample creates a popup menu with the option to create a new project
+     * @param view stores the location of where this function called from
      */
     public void popupMenuExample(View view) {
         popupButton = findViewById(R.id.popup);
@@ -148,9 +152,9 @@ public class MainActivity extends AppCompatActivity {
         p.getMenuInflater().inflate(R.menu.main_popup_menu, p .getMenu());
         p.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             /**
-             *
-             * @param item
-             * @return
+             * onMenuItemClick checks for when the user selects an option from the popup menu
+             * @param item the id of the menu option
+             * @return boolean will always return true, if false the menu stays open
              */
             public boolean onMenuItemClick(MenuItem item) {
                 //When an Item in the drop down is pressed, we need to know which one is pressed.
@@ -177,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * showAddProjectDialog prompts the user to create a name for their project
-     * @param c context !!! INSERT DESCRIPTION HERE !!!!
+     * @param c context stores the location of where this function called from
      */
     private void showAddProjectDialog(Context c) {
         final EditText taskEditText = new EditText(c);
@@ -189,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
                     /**
                      *  onClick tracks when the user clicks the text box to enter a name
                      * @param dialog shows the message "Name your new Project:" inside of the text box for the user to input a name for their new project
-                     * @param which !!!! INSERT DESCRIPTION HERE !!!!
+                     * @param which stores which button is clicked within the dialog box
                      */
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
